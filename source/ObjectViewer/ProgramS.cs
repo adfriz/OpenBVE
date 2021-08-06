17725208a5dadbd44967cc26a8195ff0730886e8
Christopher Lees <leezer3@gmail.com>
2021-08-06 21:59:22 +0100

    WIP: MSTS consist / vehicle parser

--- source/ObjectViewer/ProgramS.cs
+++ source/ObjectViewer/ProgramS.cs
@@ -268,16 +268,20 @@ namespace ObjectViewer {
 			    {
 				    if(Files[i].EndsWith(".dat", StringComparison.InvariantCultureIgnoreCase) || Files[i].EndsWith(".xml", StringComparison.InvariantCultureIgnoreCase) || Files[i].EndsWith(".cfg", StringComparison.InvariantCultureIgnoreCase))
 				    {
-					    string currentTrainFolder = System.IO.Path.GetDirectoryName(Files[i]);
+					    string currentTrain = Files[i];
+					    if (currentTrain.EndsWith("extensions.cfg", StringComparison.InvariantCultureIgnoreCase))
+					    {
+						    currentTrain = System.IO.Path.GetDirectoryName(currentTrain);
+					    }
 					    bool canLoad = false;
 					    for (int j = 0; j < Program.CurrentHost.Plugins.Length; j++)
 					    {
-						    if (Program.CurrentHost.Plugins[j].Train != null && Program.CurrentHost.Plugins[j].Train.CanLoadTrain(currentTrainFolder))
+						    if (Program.CurrentHost.Plugins[j].Train != null && Program.CurrentHost.Plugins[j].Train.CanLoadTrain(currentTrain))
 						    {
 							    Control[] dummyControls = new Control[0];
 								TrainManager.Trains = new[] { new TrainBase(TrainState.Available) };
 								AbstractTrain playerTrain = TrainManager.Trains[0];
-								Program.CurrentHost.Plugins[j].Train.LoadTrain(Encoding.UTF8, currentTrainFolder, ref playerTrain, ref dummyControls);
+								Program.CurrentHost.Plugins[j].Train.LoadTrain(Encoding.UTF8, currentTrain, ref playerTrain, ref dummyControls);
 								TrainManager.PlayerTrain = TrainManager.Trains[0];
 								canLoad = true;
 								break;
