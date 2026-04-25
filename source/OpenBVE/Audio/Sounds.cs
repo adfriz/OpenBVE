4151884d77441ff4d900c02f1b64791fc8c4fa8a
adfriz <76892624+adfriz@users.noreply.github.com>
2026-04-25 12:57:11 +0700

    Phase 4 Final: Complete OpenAL Removal and Tooling Migration
    Migrated RouteViewer and TrainEditor2 audio loops to Raylib. Removed all OpenTK.dll.config files and updated OpenBveApi documentation to remove OpenAL references. Solution is now 100% free of OpenAL dependencies.

--- source/OpenBVE/Audio/Sounds.cs
+++ source/OpenBVE/Audio/Sounds.cs
@@ -1,7 +1,6 @@
-﻿using System.Linq;
+using System.Linq;
 using OpenBveApi.Hosts;
 using OpenBveApi.Sounds;
-using OpenTK.Audio.OpenAL;
 using SoundManager;
 using TrainManager.Trains;
 
@@ -9,8 +8,6 @@ namespace OpenBve
 {
 	internal partial class Sounds : SoundsBase
 	{
-		/// <summary>Stops all sounds that are attached to the specified train.</summary>
-		/// <param name="train">The train.</param>
 		public override void StopAllSounds(object train)
 		{
 			if (!(train is TrainBase t))
@@ -19,14 +16,9 @@ namespace OpenBve
 			}
 			for (int i = 0; i < SourceCount; i++)
 			{
-				if (t.Cars.Contains(Sources[i].Parent) || Sources[i].Parent == train)
+				if (Sources[i] != null && (t.Cars.Contains(Sources[i].Parent) || Sources[i].Parent == train))
 				{
-					if (Sources[i].State == SoundSourceState.Playing)
-					{
-						AL.DeleteSources(1, ref Sources[i].OpenAlSourceName);
-						Sources[i].OpenAlSourceName = 0;
-					}
-					Sources[i].State = SoundSourceState.Stopped;
+					Sources[i].Stop();
 				}
 			}
 		}
--- source/OpenBveApi/Sounds/Sounds.SoundSourceState.cs
+++ source/OpenBveApi/Sounds/Sounds.SoundSourceState.cs
@@ -1,15 +1,15 @@
-﻿namespace OpenBveApi.Sounds
+namespace OpenBveApi.Sounds
 {
 	/// <summary>Represents the state of a sound source.</summary>
 	public enum SoundSourceState
 	{
-		/// <summary>The sound will start playing once in audible range. The OpenAL sound name is not yet valid.</summary>
+		/// <summary>The sound will start playing once in audible range. The internal sound handle is not yet valid.</summary>
 		PlayPending,
-		/// <summary>The sound is playing and the OpenAL source name is valid.</summary>
+		/// <summary>The sound is playing and the internal source handle is valid.</summary>
 		Playing,
-		/// <summary>The sound will stop playing. The OpenAL sound name is still valid.</summary>
+		/// <summary>The sound will stop playing. The internal sound handle is still valid.</summary>
 		StopPending,
-		/// <summary>The sound has stopped and will be removed from the list of sound sources. The OpenAL source name is not valid any longer.</summary>
+		/// <summary>The sound has stopped and will be removed from the list of sound sources. The internal source handle is not valid any longer.</summary>
 		Stopped,
 		/// <summary>The sound is not yet in audible range, but is paused</summary>
 		PausePending,
