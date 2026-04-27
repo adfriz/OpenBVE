e1ffa6c085e16e400e2e65dcb7a22e86541f0741
Christopher Lees <leezer3@gmail.com>
2026-04-27 10:01:50 +0100

    Fix: Missing null check for local transformation
    Caused by the helper refactor

--- source/LibRender2/BaseRenderer.cs
+++ source/LibRender2/BaseRenderer.cs
@@ -884,7 +884,7 @@ namespace LibRender2
 		public int CreateStaticObject(StaticObject Prototype, Vector3 Position, Transformation WorldTransformation, Transformation LocalTransformation, ObjectDisposalMode AccurateObjectDisposal, ObjectCreationParameters Parameters, double BlockLength)
 		{
 			Matrix4D Translate = Matrix4D.CreateTranslation(Position.X, Position.Y, -Position.Z);
-			Matrix4D Rotate = (Matrix4D)new Transformation(LocalTransformation, WorldTransformation);
+			Matrix4D Rotate = (Matrix4D)new Transformation(LocalTransformation ?? Transformation.NullTransformation, WorldTransformation);
 			return CreateStaticObject(Position, Prototype, LocalTransformation, Rotate, Translate, AccurateObjectDisposal, Parameters, BlockLength);
 		}
 
